a3059ef8f6452931d45bb61000b5765eb91c483d
Ike Maldonado <maldonadoike@gmail.com>
2022-08-16 23:18:01 -0700

    checkpoing

---
+++ events/interactionCreate.js
@@ -0,0 +1,25 @@
+import { client } from "../index.js";
+module.exports = {
+  name: "interactionCreate",
+  async execute(interaction) {
+    console.log(
+      `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+    );
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+      console.log(`Command: ${interaction.commandName}`);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  },
+};
