bb4f558ea7be7fd8fa1aa90328ec1edce82fc9e8
Ike Maldonado <maldonadoike@gmail.com>
2022-08-16 22:52:54 -0700

    refactor

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // access 'commands' in other files by attaching a commands property
 client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -17,14 +29,18 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply(`Server name: ${interaction.guild.name}`);
-  } else if (commandName === "user") {
-    await interaction.reply(`Your tag: ${interaction.user.tag}`);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
 });
 
