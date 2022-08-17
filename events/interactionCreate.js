6491d1f78f3b180e26d2a37f4f1e17d8b645c26d
Ike Maldonado <maldonadoike@gmail.com>
2022-08-17 16:14:05 -0700

    updated interactionCreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,15 +1,9 @@
-
-
 module.exports = {
   name: "interactionCreate",
-
   async execute(interaction) {
     if (!interaction.isChatInputCommand()) return;
-
     const command = interaction.client.commands.get(interaction.commandName);
-
     if (!command) return;
-
     try {
       await command.execute(interaction);
     } catch (error) {
