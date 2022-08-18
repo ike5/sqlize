c618bfcb3076c1d936ad53cfe5b31637e3846edf
Ike Maldonado <maldonadoike@gmail.com>
2022-08-17 18:06:53 -0700

    add button interaction

---
+++ commands/button.js
@@ -0,0 +1,46 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("button")
+    .setDescription("creates a button"),
+  async execute(interaction) {
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("primary")
+        .setLabel("Primary")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId("danger")
+        .setLabel("Danger")
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setURL("https://google.com")
+        .setLabel("Link")
+        .setStyle(ButtonStyle.Link),
+      new ButtonBuilder()
+        .setCustomId("success")
+        .setLabel("Success")
+        .setStyle(ButtonStyle.Success)
+    );
+
+    const embed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Some title")
+      .setURL("https://discord.js.org")
+      .setDescription("Some description here");
+
+    await interaction.reply({
+      content: "Pong!",
+      components: [row],
+      ephemeral: true,
+      embeds: [embed],
+    });
+  },
+};
