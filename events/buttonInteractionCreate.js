ef1abbe8bfa135bcedcdebf5cbe726981ac4616c
Ike Maldonado <maldonadoike@gmail.com>
2022-08-17 18:19:50 -0700

    checkpoing

--- events/buttonInteractionCreate.js
+++ events/buttonInteractionCreate.js
@@ -3,5 +3,21 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     console.log(interaction);
+
+    const filter = (i) =>
+      i.customId === "primary" && i.user.id === "911428212565885008";
+
+    const collector = interaction.channel.createMessageComponentCollector({
+      filter,
+      time: 15000,
+    });
+
+    collector.on("collect", async (i) => {
+      await i.update({ content: "A button was clicked!", components: [] });
+    });
+
+    collector.on("end", (collected) =>
+      console.log(`Collected ${collected.size} items`)
+    );
   },
 };
