2b371b59710a15434fb0fa6ac9b43e1066e9e761
Ike Maldonado <maldonadoike@gmail.com>
2022-08-17 18:11:29 -0700

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("node:fs");
 const path = require("node:path");
-// Require the necessary discord.js classes
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const { token, pingCommandId } = require("./config.json");
 
@@ -13,7 +12,6 @@ const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
-
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
@@ -25,7 +23,6 @@ for (const file of commandFiles) {
 // events
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
