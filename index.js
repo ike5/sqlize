f34f5f464e08f061503978f687a38bcf3a117b29
Ike Maldonado <maldonadoike@gmail.com>
2022-09-04 23:31:31 -0700

    commented out stuff

--- index.js
+++ index.js
@@ -30,32 +30,32 @@ const client = new Client({
 client.commands = new Collection();
 
 // Reading command files
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith('.js'));
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs
+//   .readdirSync(commandsPath)
+//   .filter((file) => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
-}
+// for (const file of commandFiles) {
+//   const filePath = path.join(commandsPath, file);
+//   const command = require(filePath);
+//   client.commands.set(command.data.name, command);
+// }
 
-// Reading event files
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith('.js'));
+// // Reading event files
+// const eventsPath = path.join(__dirname, 'events');
+// const eventFiles = fs
+//   .readdirSync(eventsPath)
+//   .filter((file) => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
+// for (const file of eventFiles) {
+//   const filePath = path.join(eventsPath, file);
+//   const event = require(filePath);
+//   if (event.once) {
+//     client.once(event.name, (...args) => event.execute(...args));
+//   } else {
+//     client.on(event.name, (...args) => event.execute(...args));
+//   }
+// }
 /**
  * Initialize database in SQLite
  */
