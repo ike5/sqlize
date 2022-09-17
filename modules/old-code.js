0ef09d7a71234af96a4dfe8aff879f5fe72f1660
Ike Maldonado <maldonadoike@gmail.com>
2022-09-16 23:04:33 -0700

    removed helper functions

--- modules/old-code.js
+++ modules/old-code.js
@@ -567,3 +567,25 @@ const ButtonInteraction = {
   
   await interaction.showModal(modal);
   
+
+
+
+
+
+  const { db } = require('../modules/initialize-models');
+
+// Checks to see if a user already exists in the database.
+async function isIdUnique(id) {
+  return await db.User.count({
+    where: {
+      discordId: id,
+    },
+  }).then((count) => {
+    if (count != 0) {
+      return false;
+    }
+    return true;
+  });
+}
+
+module.exports = { isIdUnique };
